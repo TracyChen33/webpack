e3bf5aec0b946382c457190f2d231641f546c514
TracyChen33 <tracychen3621@gmail.com>
2020-03-07 23:35:16 -0700

    update

---
+++ webpack.config.base.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.[contenthash].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Tracy--App',
+      template: 'src/assets/index.html'
+    }),
+  ]
+};
+
+
+
